f66f612734fa94fb9d7a43002379b3e04922f28e
pajri <aprilio.pajri@gmail.com>
2020-11-04 16:12:23 +0700

    - add register page functionality

--- src/components/PostForm.vue
+++ src/components/PostForm.vue
@@ -1,6 +1,6 @@
 <template>
 <div class="post-editor-container bg-secondary">
-    <b-form-textarea id="textarea-rows" placeholder="How was your day ?" rows="1" @keyup="countCharacter" :></b-form-textarea>
+    <b-form-textarea id="textarea-rows" placeholder="How was your day ?" rows="1" @keyup="countCharacter"></b-form-textarea>
     <div class="d-flex flex-row justify-content-end post-meta-container">
         <b-button variant="success" class="mr-auto align-self-center">
             <b-icon icon="card-image"></b-icon>
--- src/pages/RegisterPage.vue
+++ src/pages/RegisterPage.vue
@@ -2,105 +2,144 @@
 <div>
     <b-container>
         <div class="login-container" v-if="showRegister">
-            <b-alert v-model="showDismissibleAlert" variant="danger" dismissible>
-                Dismissible Alert!
+            <b-alert v-model="showRegAlert" variant="danger" dismissible>
+                <ul>
+                    <li v-for="m in this.registerAlertMessage" :key="m">
+                        {{m}}
+                    </li>
+                </ul>
             </b-alert>
             <b-form @submit="onRegisterSubmit">
                 <b-form-group id="input-group-full-name">
-                    <b-form-input id="txt_full_name" v-model="form.fullName" type="text" required placeholder="Enter full name"></b-form-input>
+                    <b-form-input id="txt_full_name" v-model="registrationForm.fullName" type="text" required placeholder="Enter full name"></b-form-input>
                 </b-form-group>
                 <b-form-group id="input-group-email">
-                    <b-form-input id="txt_email" v-model="form.email" type="email" required placeholder="Enter email"></b-form-input>
+                    <b-form-input id="txt_email" v-model="registrationForm.email" type="email" required placeholder="Enter email"></b-form-input>
                 </b-form-group>
 
-                <b-form-group id="input-group-password">
-                    <b-form-input type="password" id="txt_password" v-model="form.password" required placeholder="Enter password"></b-form-input>
+                <b-form-group id="input-grou~p-password">
+                    <b-form-input type="password" id="txt_password" ref="password" v-model="registrationForm.password" required placeholder="Enter password"></b-form-input>
                 </b-form-group>
 
                 <b-form-group id="input-group-password-confirmation">
-                    <b-form-input type="password" id="txt_password_confirmation" v-model="form.passwordConfirmation" required placeholder="Confirm password"></b-form-input>
+                    <b-form-input type="password" id="txt_password_confirmation" v-model="registrationForm.passwordConfirmation" required placeholder="Confirm password"></b-form-input>
                 </b-form-group>
 
                 <b-button type="submit" variant="primary">Register</b-button>
-
-                <!--read more about google login at https://www.npmjs.com/package/vue-google-login -->
-                <GoogleLogin :params="googleLoginParams" :renderParams="goggleLoginRenderParams"></GoogleLogin>
             </b-form>
         </div>
 
-        <div class="login-container" v-if="showConfirmation">
-            <b-alert v-model="showDismissibleAlert" variant="danger" dismissible>
-                Dismissible Alert!
-            </b-alert>
-            <b-form @submit="onOtpSubmit">
-                <b-form-group id="input-group-email">
-                    <b-form-input id="txt_email" v-model="form.email" type="email" disabled></b-form-input>
-                </b-form-group>
-
-                <b-form-group id="input-group-password-confirmation">
-                    <b-form-input type="number" id="txt_number" v-model="form.otp" required placeholder="Confirm password"></b-form-input>
-                </b-form-group>
-
-                <b-button type="submit" variant="primary">Submit</b-button>
-
-                <!--read more about google login at https://www.npmjs.com/package/vue-google-login -->
-                <GoogleLogin :params="googleLoginParams" :renderParams="goggleLoginRenderParams"></GoogleLogin>
-            </b-form>
+        <div class="login-container" v-if="showRegSuccesfull">
+            Registration successful ! <br />
+            Please check your email to verify your email. <br>
+            <a href="/login">Go to login page</a>
         </div>
     </b-container>
 </div>
 </template>
 
 <script>
+import axios from "axios";
 import {
     BAlert,
     BContainer
 } from "bootstrap-vue";
 
-import {
-    GoogleLogin
-} from "vue-google-login";
-
 export default {
     name: "LoginPage",
     data() {
         return {
-            form: {
-                full_name: "",
+            registrationForm: {
+                fullName: "",
                 email: "",
                 password: "",
-                txt_password_confirmation: "",
-                otp: 0,
+                passwordConfirmation: "",
             },
-            googleLoginParams: {
-                client_id: "xxxxxx",
-            },
-            // only needed if you want to render the button with the google ui
-            goggleLoginRenderParams: {
-                width: 250,
-                height: 50,
-                longtitle: true,
-            },
-            showDismissibleAlert: true,
+            registerAlertMessage: [],
+            showRegAlert: false,
             showRegister: true,
-            showConfirmation: false,
+            showRegSuccesfull: false
         };
     },
     components: {
         BAlert,
         BContainer,
-        GoogleLogin,
     },
     methods: {
         onRegisterSubmit(evt) {
-            evt.preventDefault()
-            this.showRegister = false
-            this.showConfirmation = true
+            evt.preventDefault();
+
+            let registrationValidation = this.validateRegistrationForm();
+            if (!registrationValidation.isValid) {
+                this.showRegistrationAlert(registrationValidation.message);
+                return;
+            }
+
+            /*start prepare request*/
+            var registerUrl = process.env.VUE_APP_API_HOST + "/api/auth/signup";
+            var config = {
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded'
+                }
+            };
+
+            var data = new URLSearchParams();
+            data.append('full_name', this.registrationForm.fullName);
+            data.append('email', this.registrationForm.email);
+            data.append('password', this.registrationForm.password);
+            data.append('confirm_password', this.registrationForm.passwordConfirmation);
+            /*end prepare request*/
+
+            /*start request execution*/
+            var self = this;
+            axios
+                .post(registerUrl, data, config)
+                .then((response) => {
+                    if (response.status == 201) {
+                        self.showRegister = false;
+                        self.showRegSuccesfull = true;
+                    } else {
+                        console.error("undefined status code : " + response.status)
+                    }
+
+                })
+                .catch(function (error) {
+                    self.showRegistrationAlert(error.response.data.message)
+                });
+            /*end request execution*/
+
         },
         onOtpSubmit(evt) {
-            evt.preventDefault()
-            window.location.href = 'login'
+            evt.preventDefault();
+            window.location.href = "login";
+        },
+        validateRegistrationForm() {
+            let isValid = true;
+            let message = []
+            //validate password confirmation
+            if (
+                this.registrationForm.password !=
+                this.registrationForm.passwordConfirmation
+            ) {
+                isValid = false;
+                message.push("Password confirmation does not match.");
+            }
+
+            //validate password length
+            if (this.registrationForm.password.length < 10) {
+                isValid = false;
+                message.push("Password length minimum 10 characters.")
+            }
+
+            return {
+                isValid: isValid,
+                message: message,
+            };
+        },
+        showRegistrationAlert(val) {
+            this.showRegAlert = true;
+            this.registerAlertMessage = val;
         }
-    },
+    }
 };
 </script>
