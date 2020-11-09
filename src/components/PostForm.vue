79b917d25120a63630ec747d8eb4133cb1b165e5
pajri <aprilio.pajri@gmail.com>
2020-11-09 16:50:10 +0700

    - add upload image functionality - set text post to required - validate image extensions

--- src/components/PostForm.vue
+++ src/components/PostForm.vue
@@ -9,11 +9,13 @@
     </b-alert>
     <div class="post-editor-container bg-secondary">
         <b-form @submit="onSubmit">
-            <b-form-textarea id="textarea-rows" placeholder="How was your day ?" rows="5" @keyup="countCharacter" v-model="postText" :state="postText.length <= this.maxChar"></b-form-textarea>
+            <b-form-textarea id="textarea-rows" placeholder="How was your day ?" rows="5" @keyup="countCharacter" v-model="postText" :state="postText.length <= this.maxChar" required></b-form-textarea>
             <div class="d-flex flex-row justify-content-end post-meta-container">
-                <b-button variant="success" class="mr-auto align-self-center">
+                <b-button variant="success" class="align-self-center input-image" @click="onUploadClick">
                     <b-icon icon="card-image"></b-icon>
                 </b-button>
+                <input type="file" ref="frmImage" class="form-image-upload" @change="onFileSelect" :accept="accept">
+                <span class="align-self-center mr-auto">{{fileName}}</span>
 
                 <span class="align-self-center">{{ charCount }}</span>
                 <b-button type="submit" variant="success" :disabled='disableButtonPost'>Post !</b-button>
@@ -38,10 +40,17 @@ export default {
             maxChar: 500,
             charCount: 500,
             postText: "",
+            fileName: "",
             showAlert: false,
             errorMessage: [],
             disableButtonPost: false,
+            allowedFileExtension: ['.bmp', '.gif', '.jpeg', '.jpg', '.png', '.tiff'],
+            accept: "",
+            imageUrl: "",
         };
+    },
+    mounted() {
+        this.accept = this.allowedFileExtension.join(",");
     },
     methods: {
         countCharacter() {
@@ -58,29 +67,11 @@ export default {
             this.hideErrorMessage();
             this.disableButtonPost = true;
 
-            const postURL = process.env.VUE_APP_API_HOST + "/api/post";
-
-            const data = new URLSearchParams()
-            data.append("content", this.postText);
-
-            const config = {
-                withCredentials: true,
-                headers: {
-                    'Authorization': localStorage.JWT
-                }
-            };
-
-            axios
-                .post(postURL, data, config)
-                .then((response) => {
-                    if (response.status == 201) {
-                        this.clearForm();
-                    }
-                })
-                .catch((error) => {
-                    this.showErrorMessage(error.response.data.message);
-                })
-                .finally(() => this.disableButtonPost = false)
+            if (this.$refs.frmImage.files.length > 0) {
+                this.postContentWithImage();
+            } else {
+                this.postContent();
+            }
 
         },
         cleanInput() {
@@ -96,6 +87,86 @@ export default {
         },
         clearForm() {
             this.postText = "";
+            this.fileName = "";
+            this.$refs.frmImage.value = "";
+        },
+        onUploadClick(evt) {
+            evt.preventDefault();
+            this.$refs.frmImage.click()
+        },
+        onFileSelect(evt) {
+            const file = evt.target.files;
+            if (file.length > 0) {
+                //get file extension
+                let filename = file[0].name;
+                const ext = "." + filename.split(".").pop();
+
+                //validate allowed file extenison
+                if (this.allowedFileExtension.includes(ext)) {
+                    this.fileName = file[0].name;
+                } else {
+                    this.showErrorMessage(['File type is not allowed.']);
+                    this.$refs.frmImage.value = '';
+                }
+            }
+        },
+        postContent() {
+            const postURL = process.env.VUE_APP_API_HOST + "/api/post";
+
+            const data = new FormData();
+            data.append('content', this.postText);
+            if (this.$refs.frmImage.files.length > 0) {
+                data.append('image_url', this.imageUrl);
+            }
+
+            const config = {
+                withCredentials: true,
+                headers: {
+                    'Authorization': localStorage.JWT
+                }
+            };
+
+            axios
+                .post(postURL, data, config)
+                .then((response) => {
+                    if (response.status == 201) {
+                        this.imageUrl = response.data.image_url;
+                        this.clearForm();
+                    }
+                })
+                .catch((error) => {
+                    this.showErrorMessage(error.response.data.message);
+                })
+                .finally(() => this.disableButtonPost = false)
+        },
+        postContentWithImage() {
+            const uploadImageURL = process.env.VUE_APP_API_HOST + "/api/image";
+
+            const data = new FormData()
+            data.append("image", this.$refs.frmImage.files[0]);
+
+            const config = {
+                withCredentials: true,
+                headers: {
+                    'Authorization': localStorage.JWT
+                }
+            };
+
+            let self = this;
+            axios
+                .post(uploadImageURL, data, config)
+                .then((response) => {
+                    if (response.status == 200) {
+                        self.imageUrl = response.data.image_url;
+                    }
+                })
+                .then(() => {
+                    self.postContent();
+                })
+                .catch((error) => {
+                    self.showErrorMessage(error.response.data.message);
+                    self.disableButtonPost = false;
+                })
         }
     },
 };
