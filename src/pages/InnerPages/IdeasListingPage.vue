a16c0af7f36f5a37878825eea889f04dc23ce4b9
pajri <aprilio.pajri@gmail.com>
2020-11-25 21:48:05 +0700

    - change logic for listing loading trigger

--- src/pages/InnerPages/IdeasListingPage.vue
+++ src/pages/InnerPages/IdeasListingPage.vue
@@ -4,15 +4,7 @@
       <b-col md="12" class="background-independence">
         <Sidebar :navbar-height="navbarHeight" v-if="false" />
         <PostForm @onPosted="insertPost" />
-        <IdeasListing
-          :postList="postList"
-          :errorMessage="listingErrorMessage"
-          :showAlert="isListingAlertShow"
-          :showSpinner="isListingSpinnerShow"
-          @onDeleteSuccess="onDeleteSuccess"
-          @onDeleteError="onDeleteError"
-          @onDismissed="hideListingErrorAlert"
-        />
+        <IdeasListing :postList="postList" :errorMessage="listingErrorMessage" :showAlert="isListingAlertShow" :showSpinner="isListingSpinnerShow" @onDeleteSuccess="onDeleteSuccess" @onDeleteError="onDeleteError" @onDismissed="hideListingErrorAlert" />
       </b-col>
     </b-row>
   </b-container>
@@ -21,7 +13,7 @@
 <script>
 import axios from "axios";
 import { getAccessToken } from "@/module/auth_util";
-import { generateErrorMessageFromResponse } from "@/module/axios_util"
+import { generateErrorMessageFromResponse } from "@/module/axios_util";
 
 import Sidebar from "../../components/Sidebar.vue";
 import IdeasListing from "../../components/IdeasListing.vue";
@@ -77,10 +69,7 @@ export default {
       axios
         .get(getListUrl + "", config)
         .then((response) => {
-          if (
-            response.data.post_list != null &&
-            response.data.post_list.length > 0
-          ) {
+          if (response.data.post_list != null && response.data.post_list.length > 0) {
             this.postList = this.postList.concat(response.data.post_list);
           }
         })
@@ -106,18 +95,13 @@ export default {
       this.isListingAlertShow = true;
     },
     handleScroll() {
-      const scrollTop =
-        (document.documentElement && document.documentElement.scrollTop) ||
-        document.body.scrollTop;
-      const scrollHeight =
-        (document.documentElement && document.documentElement.scrollHeight) ||
-        document.body.scrollHeight;
+      const scrollTop = (document.documentElement && document.documentElement.scrollTop) || document.body.scrollTop;
+      const scrollHeight = (document.documentElement && document.documentElement.scrollHeight) || document.body.scrollHeight;
       const clientHeight = document.documentElement.clientHeight;
 
       const scrolledToBottom = scrollTop >= scrollHeight - clientHeight - 100;
-      if (scrolledToBottom && !this.isListingSpinnerShow && this.postList.length > 10) {
-        const lastPostDate = this.postList[this.postList.length - 1]
-          .hidden_date;
+      if (scrolledToBottom && !this.isListingSpinnerShow && this.postList.length >= 10) {
+        const lastPostDate = this.postList[this.postList.length - 1].hidden_date;
         this.loadPostList(5, lastPostDate);
       }
     },
